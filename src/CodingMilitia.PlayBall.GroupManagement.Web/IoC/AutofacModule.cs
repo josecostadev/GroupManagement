5d6cf924976e2e344005c0ff7ea65855bc004449
josecostadev <89701911+josecostadev@users.noreply.github.com>
2021-08-29 23:23:02 +0100

    Episode 005

---
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/IoC/AutofacModule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using CodingMilitia.PlayBall.GroupManagement.Business.Impl.Services;
+using CodingMilitia.PlayBall.GroupManagement.Business.Models;
+using CodingMilitia.PlayBall.GroupManagement.Business.Services;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //builder.RegisterType<InMemoryGroupsService>().As<IGroupsService>().SingleInstance();
+
+            builder.RegisterDecorator<IGroupsService>((context, service) => new GroupsServiceDecorator(service), "groupsService");
+            builder.RegisterType<InMemoryGroupsService>().Named<IGroupsService>("groupsService").SingleInstance();
+        }
+
+        private class GroupsServiceDecorator : IGroupsService
+        {
+            private readonly IGroupsService _inner;
+
+            public GroupsServiceDecorator(IGroupsService inner)
+            {
+                _inner = inner;
+            }
+
+            public Group Add(Group group)
+            {
+                Console.WriteLine($"##### Add");
+                return _inner.Add(group);
+            }
+
+            public IReadOnlyCollection<Group> GetAll()
+            {
+                Console.WriteLine("##### GetAll");
+                return _inner.GetAll();
+            }
+
+            public Group GetById(long id)
+            {
+                Console.WriteLine($"##### GetById {id}");
+                return _inner.GetById(id);
+            }
+
+            public Group Update(Group group)
+            {
+                Console.WriteLine($"##### Update");
+                return _inner.Update(group);
+            }
+        }
+    }
+}
--- src/CodingMilitia.PlayBall.GroupManagement.Web/Startup.cs
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/Startup.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Threading.Tasks;
 
 namespace CodingMilitia.PlayBall.GroupManagement.Web
@@ -9,14 +12,33 @@ namespace CodingMilitia.PlayBall.GroupManagement.Web
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
 
-            // Dependency Injection (ony by one)
-            //services.AddSingleton<IGroupsService, InMemoryGroupsService>();
+            // Removed the default DI container
+            //services.AddBusiness();
 
-            services.AddBusiness();
+            // Add services to the collection
+            services.AddOptions();
+
+            // Create a container-builder and register dependencies
+            var containerBuilder = new ContainerBuilder();
+
+            // Register your own things directly with Autofac
+            containerBuilder.RegisterModule<AutofacModule>();
+
+            // Populate the service-descriptors added to `IServiceCollection`
+            // BEFORE you add things to Autofac so that the Autofac
+            // registrations can override stuff in the `IServiceCollection`
+            // as needed
+            containerBuilder.Populate(services);
+
+            var autofacContainer = containerBuilder.Build();
+
+            // this will be used as the service-provider for the application!
+            return new AutofacServiceProvider(autofacContainer);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
