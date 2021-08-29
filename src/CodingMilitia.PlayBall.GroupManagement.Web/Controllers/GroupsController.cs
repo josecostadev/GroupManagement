3b5b9fb3043a01e0f595bd5db8b1b7761963faa0
josecostadev <89701911+josecostadev@users.noreply.github.com>
2021-08-29 19:02:59 +0100

    Episode 003

---
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/Controllers/GroupsController.cs
@@ -0,0 +1,93 @@
+﻿using CodingMilitia.PlayBall.GroupManagement.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingMilitia.PlayBall.GroupManagement.Web.Controllers
+{
+    [Route("groups")]
+    public class GroupsController : Controller
+    {
+        private static long currentGroupId = 1;
+        private static IList<GroupViewModel> groups = new List<GroupViewModel>
+        {
+            new GroupViewModel
+            {
+                Id = currentGroupId++,
+                Name = "Group 1 Name"
+            }
+        };
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View(groups);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Details(long id, string extra)
+        {
+            var group = groups.SingleOrDefault(o => o.Id == id);
+
+            if(group == null)
+            {
+                return NotFound();
+            }
+
+            if(!string.IsNullOrEmpty(extra))
+            {
+                var newGroup = new GroupViewModel
+                {
+                    Id = group.Id,
+                    Name = $"{group.Name}. Extra: {extra}"
+                };
+
+                group = newGroup;
+            }
+
+            return View(group);
+        }
+
+        [HttpPost]
+        [Route("{id}")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(long id, GroupViewModel model)
+        {
+            var group = groups.SingleOrDefault(o => o.Id == id);
+
+            if (group == null)
+            {
+                return NotFound();
+            }
+
+            group.Name = model.Name;
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(GroupViewModel model)
+        {
+            var group = new GroupViewModel
+            {
+                Id = currentGroupId++,
+                Name = model.Name
+            };
+
+            groups.Add(group);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
